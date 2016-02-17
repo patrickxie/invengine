78c5c81c09012c17832b67c90139c7bca9ade579
Patrick Xie <Patrick.y.xie@gmail.com>
2016-02-16 18:02:13 -0800

    added auth setup

--- client/pages/Bar.js
+++ client/pages/Bar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
 export default function Bar() {
+        // console.log('uri: ', state.routing.location.pathname);
   return <div>And I am Bar!</div>
 }
--- client/pages/export.js
+++ client/pages/export.js
@@ -1,3 +1,4 @@
 export Foo from './Foo';
 export Bar from './Bar';
+export Auth from './Auth';
 export Index from './Index';
