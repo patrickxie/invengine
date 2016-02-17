430dd7de54ac860403b011dfa92cc93b0d8a2f43
Patrick Xie <Patrick.y.xie@gmail.com>
2016-02-17 07:37:29 -0800

    changed npm & node version so that heroku app can install react without installing multiple versions

--- client/pages/Auth.jsx
+++ client/pages/Auth.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import Divider from 'material-ui/lib/divider';
 
-export default function Auth() {
+import Menu from 'material-ui/lib/menus/menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
-  return <div>I am Auth!</div>
-}
+const style = {
+  marginRight: 32,
+  marginBottom: 32,
+  float: 'left',
+  position: 'relative',
+  zIndex: 0,
+};
 
+const MenuExampleSimple = () => (
+  <div>
+  <h3>Step 1</h3>
+<Divider inset={true}/>
+    <Menu style={style}>
+      <MenuItem primaryText="Maps" />
+      <MenuItem primaryText="Books" />
+      <MenuItem primaryText="Flights" />
+      <MenuItem primaryText="Apps" />
+    </Menu>
+    <Menu style={style}>
+      <MenuItem primaryText="Refresh" />
+      <MenuItem primaryText="Help &amp; feedback" />
+      <MenuItem primaryText="Settings" />
+      <MenuItem primaryText="Sign out" />
+    </Menu>
+  </div>
+);
 
+export default MenuExampleSimple;
+
+// import React from 'react'
+
+// export default function Auth() {
+//   return <div>And I am Auth!</div>
+// }
