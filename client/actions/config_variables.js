f252f866e0e0206bb67ac15a0b357517a5021a2b
Patrick Xie <Patrick.y.xie@gmail.com>
2016-03-17 10:43:44 -0700

    setting up backend dev

--- client/actions/config_variables.js
+++ client/actions/config_variables.js
@@ -12,7 +12,7 @@ export function sendInvites() {
   return async (dispatch, getState) => {
     let { assistvars, configvars } = getState();
     //add USER_ID: configvars.USER_ID here
-    let data = { url:configvars.url, invites: assistvars };
+    let data = { url:configvars.url, invites: assistvars, owner:'joe' };
     dispatch({
       type: 'send_invites',
     });
