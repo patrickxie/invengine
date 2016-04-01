ddc9cad3097df4ef8a95920575094a6cb95a7120
Patrick Xie <Patrick.y.xie@gmail.com>
2016-04-01 16:16:31 -0700

    changed so that id is appropriately added into the data being dispatched to invite endpoint

--- client/actions/config_variables.js
+++ client/actions/config_variables.js
@@ -34,7 +34,9 @@ export function sendInvites() {
       try {
         const result = await get(`http://localhost:5000/api/users`);
         // const result = await get(`/api/users/${data.id}`);//uncomment this for local web
-        data = { ...data, id: result.id, token:result.token };
+        console.log('aResult: ', result)
+        data = { ...data, id: result.invengine_id, token:result.token };
+        console.log('newdata: ', data)
         dispatch({
           type:'obtaining_id&token_from_server_success',
           id: result.invengine_id, token: result.token
@@ -50,42 +52,6 @@ export function sendInvites() {
     }
   }
 }
-    // if ( !data.id && !data.token ){
-    //   dispatch({type:getting id and token})
-    //   try{
-    //       const result = await get(`/api/users/${data.id}`)
-    //       data.id = result.id
-    //       data.token = result.token
-
-    //       dispatch token success
-    //       dispatch(dispatchSendInvites(data))
-    //   }
-    //   catch(e){
-    //       dispatch token failure
-    //   }
-    // }
-    // let id = 'zedshen';
-    // console.log('id is: ', id)
-    // console.log(JSON.stringify(data));
-
-
-    // dispatch({
-    //   type: 'send_invites',
-    // });
-
-    // try {
-    //   const result = await post(`http://localhost:5000/api/invites/${id}`, data);
-    //   // const result = await post(`/api/invites/${id}`, data); //uncomment this after implmemtning python api endpoint logic
-    //   dispatch({
-    //     type: 'send_invites_success',
-    //     USER_ID: result.id
-    //   });
-    // } catch(e) {
-    //   dispatch({
-    //     type: 'send_invites_failure',
-    //     error: e
-    //   });
-    // }
 
 
 export function dispatchSendInvites(data) {
