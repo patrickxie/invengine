851d2ce3a8043c7a1412c264bef9bbbc257d50ab
Patrick Xie <Patrick.y.xie@gmail.com>
2016-04-07 22:24:03 -0700

    added adding_email into reducer

--- client/reducers/config_variables.js
+++ client/reducers/config_variables.js
@@ -3,18 +3,20 @@ const default_message = 'Hey, I am sharing this link to you via invengine.co'
 const DEFAULT_STATE = { url: '', details:[], invengine_id:'',
   token:'', message: default_message };
 
-
 const url = (state, action) => ({
   ...state,
   url : action.url
+});
+
+const mail = (state, action) => ({
+  ...state,
+  details : [{ email : [{ address: action.mail }] }]
 });
 
 const message = (state, action) => ({
   ...state,
   message : action.message
 });
-
-
 
 const details = (state, action) => ({
   ...state,
@@ -25,7 +27,7 @@ const reset = (state, action) => ({
   ...state,
   message: default_message,
   url: '',
-  invite_done: action.invite_done
+  invite_done: action.invite_done //remove
 })
 
 const addToken = (state, action) => ({
@@ -37,6 +39,7 @@ const addToken = (state, action) => ({
 export default function to_invite_list(state = DEFAULT_STATE, action) {
   return ({
     ['input_url']: url,
+    ['input_email']: mail,
     ['input_custom_invite_message']: message,
     ['add_import_peripheral_details']: details,
     ['send_invites_success']: reset,
